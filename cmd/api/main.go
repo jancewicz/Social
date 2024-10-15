f9e8fff4991a6e33a8bb442865bfa9eea03f9634
jancewicz <jancewicz.pawel1@gmail.com>
2024-10-15 18:13:17 +0200

    added basic db config with opening db connection logic

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/jancewicz/social/internal/db"
 	"github.com/jancewicz/social/internal/env"
 	"github.com/jancewicz/social/internal/store"
 	"github.com/joho/godotenv"
@@ -17,9 +18,25 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString(os.Getenv("SRV_ADDR"), ":8080"),
+		db: dbConfig{
+			addr:         env.GetString(os.Getenv("DB_ADDR"), "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt(os.Getenv("DB_MAX_OPEN_CONNS"), 30),
+			maxIdleConns: env.GetInt(os.Getenv("DB_MAX_IDLE_CONNS"), 30),
+			maxIdleTime:  env.GetString(os.Getenv("DB_MAX_IDLE_TIME"), "15min"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
