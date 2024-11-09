9f232a2c43ff8278b34162fe6ddcd431b3491ee9
jancewicz <jancewicz.pawel1@gmail.com>
2024-11-09 23:20:07 +0100

    fixed same typo in compose yml

--- cmd/migrate/migrations/000001_create_users.up.sql
+++ cmd/migrate/migrations/000001_create_users.up.sql
@@ -2,8 +2,8 @@ CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE IF NOT EXISTS users(
     id bigserial PRIMARY KEY,
-    email citex UNIQUE NOT NULL,
-    username varchar(255) UNIQUE NOT NULL,
+    email citext UNIQUE NOT NULL,
+    username citext UNIQUE NOT NULL,
     password bytea NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
