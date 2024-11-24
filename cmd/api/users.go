210dd2f2688f6d8016e9b8b754e851080d8cbb73
jancewicz <jancewicz.pawel1@gmail.com>
2024-11-24 18:34:34 +0100

    added user middleware to fetch user by id

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"strconv"
 
@@ -8,28 +9,55 @@ import (
 	"github.com/jancewicz/social/internal/store"
 )
 
+type userKey string
+
+const userCtx userKey = "user"
+
 func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	userID, err := strconv.ParseInt(chi.URLParam(r, "userId"), 10, 64)
-	if err != nil {
-		app.badRequestError(w, r, err)
-		return
-	}
-
-	ctx := r.Context()
-
-	user, err := app.store.Users.GetUserByID(ctx, userID)
-	if err != nil {
-		switch err {
-		case store.ErrNotFound:
-			app.notFoundError(w, r, err)
-			return
-		default:
-			app.internalSeverError(w, r, err)
-			return
-		}
-	}
+	user := getUserFromCtx(r)
 
 	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
 		app.internalSeverError(w, r, err)
 	}
+}
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "userId")
+		userID, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			app.badRequestError(w, r, err)
+		}
+
+		ctx := r.Context()
+
+		user, err := app.store.Users.GetUserByID(ctx, userID)
+		if err != nil {
+			switch err {
+			case store.ErrNotFound:
+				app.notFoundError(w, r, err)
+				return
+			default:
+				app.internalSeverError(w, r, err)
+				return
+			}
+		}
+
+		ctx = context.WithValue(ctx, userCtx, user)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getUserFromCtx(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userCtx).(*store.User)
+	return user
 }
