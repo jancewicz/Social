ead1637d3b22a90cb581318418bab374dd3d0b46
jancewicz <jancewicz.pawel1@gmail.com>
2024-11-26 22:35:54 +0100

    created new error for status 409 conflict error

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -6,19 +6,25 @@ import (
 )
 
 func (app *application) internalSeverError(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+	log.Printf("internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err)
 
 	writeJSONError(w, http.StatusInternalServerError, "the server encountered problem")
 }
 
 func (app *application) badRequestError(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("bad request error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+	log.Printf("bad request error: %s path: %s error: %s", r.Method, r.URL.Path, err)
 
 	writeJSONError(w, http.StatusBadRequest, err.Error())
 }
 
 func (app *application) notFoundError(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("not found error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+	log.Printf("not found error: %s path: %s error: %s", r.Method, r.URL.Path, err)
 
 	writeJSONError(w, http.StatusNotFound, "not found")
+}
+
+func (app *application) conflictError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("conflict error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+
+	writeJSONError(w, http.StatusConflict, err.Error())
 }
