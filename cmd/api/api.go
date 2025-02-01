79c2b20582eed1821cceb87f039cbf3e4f750899
jancewicz <jancewicz.pawel1@gmail.com>
2025-02-01 19:20:15 +0100

    changed order of middleware and CHI-CORS middleware in api.go

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -15,6 +15,7 @@ import (
 	"github.com/go-chi/cors"
 	"github.com/jancewicz/social/docs"
 	"github.com/jancewicz/social/internal/auth"
+	"github.com/jancewicz/social/internal/env"
 	"github.com/jancewicz/social/internal/mailer"
 	"github.com/jancewicz/social/internal/ratelimiter"
 	"github.com/jancewicz/social/internal/store"
@@ -88,21 +89,19 @@ type dbConfig struct {
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
 
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedOrigins:   []string{env.GetString(("ALLOWED_CORS_FRONTEND_URL"), "http://localhost:5173/")},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
 	r.Use(app.RateLimiterMiddleware)
-
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Route("/v1", func(r chi.Router) {
